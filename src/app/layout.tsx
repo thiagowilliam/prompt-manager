ac56353f924ce210183dbfd13d30e51e2f80179d
Thiago william <thiago.will.silva@gmail.com>
2026-05-27 19:49:18 -0300

    feat: sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
+import { Sidebar } from 'lucide-react';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} antialiased bg-gray-900 text-white`}>
         <section className="flex h-screen">
+          <Sidebar />
           <main className="relative flex-1 overflow-auto min-w-0">
             <div className="p-4 sm:p-6 md:p-8 max-w-full md:max-w-3xl mx-auto h-full">
               {children}
